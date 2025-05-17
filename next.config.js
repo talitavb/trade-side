805b77be7c6b14eb1f79c7579a8f1566736d1c4f
talitavb <talitavborges@gmail.com>
2025-05-17 13:56:34 -0300

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/trade-side', // trade-site
 }
 
 module.exports = nextConfig
+
